294aedacdd57f32760f85e96cd8f55ff584790b6
Tyler Talbert <tylertalbert02@gmail.com>
2025-03-22 13:25:23 -0400

    Initital commit

--- src/main/java/com/socialmedia/rest/webservices/restfulwebservices/SocialMediaRestWebservicesApplication.java
+++ src/main/java/com/socialmedia/rest/webservices/restfulwebservices/SocialMediaRestWebservicesApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SocialMediaRestWebservicesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SocialMediaRestWebservicesApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SocialMediaRestWebservicesApplication.class, args);
+    }
 
 }
--- src/test/java/com/socialmedia/rest/webservices/restfulwebservices/SocialMediaRestWebservicesApplicationTests.java
+++ src/test/java/com/socialmedia/rest/webservices/restfulwebservices/SocialMediaRestWebservicesApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SocialMediaRestWebservicesApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
